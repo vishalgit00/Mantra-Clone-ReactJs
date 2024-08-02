17c2c6f4564dfa43b7f9643b1551bc753c9a9418
vishalgit00 <vishalprmr888@gmail.com>
2024-08-02 11:18:52 +0530

    Add files via upload

---
+++ src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import HomeItems from "../components/HomeItems";
+
+
+const Home = () => {
+  const items = useSelector((store)=>store.items);
+  console.log("Got Items",items)
+    return (
+        <main>
+        <div className="items-container">
+          {items.map(item=><HomeItems key={item.id} item={item}/>)}
+          </div>  
+        </main> 
+      );
+};
+export default Home;
